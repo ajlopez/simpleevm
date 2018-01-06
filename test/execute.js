e1c5fa8a6c43de030038c7abd12051c789ea1793
ajlopez <webmaster@ajlopez.com>
2018-01-06 17:52:31 -0300

    Push three bytes; push opcode implementation refactor

--- test/execute.js
+++ test/execute.js
@@ -1,7 +1,7 @@
 
 var simpleevm = require('..');
 
-exports['execute push'] = function (test) {
+exports['execute push one byte'] = function (test) {
 	var status = simpleevm.execute("6001");
 	
 	test.ok(status);
@@ -10,3 +10,21 @@ exports['execute push'] = function (test) {
 	test.equal(status.stack.pop(), 1);
 }
 
+exports['execute push two bytes'] = function (test) {
+	var status = simpleevm.execute("610102");
+	
+	test.ok(status);
+	test.ok(status.stack);
+	test.equal(status.stack.size(), 1);
+	test.equal(status.stack.pop(), 258);
+}
+
+
+exports['execute push three bytes'] = function (test) {
+	var status = simpleevm.execute("62010000");
+	
+	test.ok(status);
+	test.ok(status.stack);
+	test.equal(status.stack.size(), 1);
+	test.equal(status.stack.pop(), 256 * 256);
+}
