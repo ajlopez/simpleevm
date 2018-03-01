006bf968fad8d2a63edbf1364b8143f2314f6b42
ajlopez <webmaster@ajlopez.com>
2018-03-01 12:08:34 -0300

    Convert to bytes with expected length

--- test/bytes.js
+++ test/bytes.js
@@ -11,6 +11,19 @@ exports['convert simple integer to bytes'] = function (test) {
 	test.equal(1, result[1]);
 };
 
+exports['convert simple integer to bytes with expected length'] = function (test) {
+	var result = bytes.toBytes(258, 32);
+	
+	test.ok(result);
+	test.ok(Array.isArray(result));
+	test.equal(32, result.length);
+	test.equal(2, result[0]);
+	test.equal(1, result[1]);
+	
+	for (var k = 2; k < 32; k++)
+		test.equal(0, result[k]);
+};
+
 exports['convert hexa string to bytes'] = function (test) {
 	var result = bytes.toBytes("0x01ff");
 	
@@ -21,3 +34,15 @@ exports['convert hexa string to bytes'] = function (test) {
 	test.equal(1, result[1]);
 };
 
+exports['convert hexa string to bytes with expected length'] = function (test) {
+	var result = bytes.toBytes("0x01ff", 32);
+	
+	test.ok(result);
+	test.ok(Array.isArray(result));
+	test.equal(32, result.length);
+	test.equal(255, result[0]);
+	test.equal(1, result[1]);
+	
+	for (var k = 2; k < 32; k++)
+		test.equal(result[k], 0);
+};
