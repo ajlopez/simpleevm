795e325d9b446d6bb71557518f1582ce0972254b
ajlopez <webmaster@ajlopez.com>
2018-05-23 16:45:15 -0300

    Additional test, gt opcode gas cost

--- test/execute.js
+++ test/execute.js
@@ -282,6 +282,8 @@ exports['execute two pushes and gt given false'] = function (test) {
 	test.ok(state.stack);
 	test.equal(state.stack.size(), 1);
 	test.equal(state.stack.pop(), 0);
+
+	test.equal(state.gasused, 3 + 3 + 3);
 }
 
 exports['execute two pushes and gt given false when equal'] = function (test) {
