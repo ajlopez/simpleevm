151cf0dfe60c12dcb7f6eefe27635836132f44c2
ajlopez <webmaster@ajlopez.com>
2018-12-15 14:31:19 -0300

    DataWord from integer value

--- test/datawords.js
+++ test/datawords.js
@@ -20,6 +20,52 @@ exports['get bytes'] = function (test) {
         test.equal(result[k], 0);
 };
 
+exports['from byte value'] = function (test) {
+    var dataword = datawords.dataword(1);
+    
+    var result = dataword.bytes();
+    
+    test.ok(result);
+    test.equal(result.length, datawords.BYTES);
+    
+    for (var k = 0; k < datawords.BYTES - 1; k++)
+        test.equal(result[k], 0);
+    
+    test.equal(result[datawords.BYTES - 1], 1);
+};
+
+exports['from short value'] = function (test) {
+    var dataword = datawords.dataword(256);
+    
+    var result = dataword.bytes();
+    
+    test.ok(result);
+    test.equal(result.length, datawords.BYTES);
+    
+    for (var k = 0; k < datawords.BYTES - 2; k++)
+        test.equal(result[k], 0);
+    
+    test.equal(result[datawords.BYTES - 2], 1);
+    test.equal(result[datawords.BYTES - 1], 0);
+};
+
+exports['from uint32 value'] = function (test) {
+    var dataword = datawords.dataword(0xffffffff);
+    
+    var result = dataword.bytes();
+    
+    test.ok(result);
+    test.equal(result.length, datawords.BYTES);
+    
+    for (var k = 0; k < datawords.BYTES - 4; k++)
+        test.equal(result[k], 0);
+    
+    test.equal(result[datawords.BYTES - 4], 0xff);
+    test.equal(result[datawords.BYTES - 3], 0xff);
+    test.equal(result[datawords.BYTES - 2], 0xff);
+    test.equal(result[datawords.BYTES - 1], 0xff);
+};
+
 exports['to string'] = function (test) {
     var dataword = datawords.dataword();
     
